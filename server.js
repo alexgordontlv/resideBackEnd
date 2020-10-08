15ec040de3712d508af54e679e309e0a89dcbf00
Alexander Gordon <62938794+alexgordontlv@users.noreply.github.com>
2020-10-08 10:49:15 +0300

    truing pg

--- server.js
+++ server.js
@@ -93,10 +93,7 @@ const propertydatabase = [
     },
 ]
 
-app.get('*', function (req, res) {
-    const index = path.join(__dirname, 'build', 'index.html');
-    res.sendFile(index);
-  });
+
 
 app.get("/", (req,res)=>{
     res.send("working with heroku")
