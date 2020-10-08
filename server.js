8f719c2ddfb9c9bee75d43ef56b33621b8b75042
Alexander Gordon <62938794+alexgordontlv@users.noreply.github.com>
2020-10-08 09:50:41 +0300

    truing pg

--- server.js
+++ server.js
@@ -161,14 +161,15 @@ app.post('/Register', (req,res) => {
     const hash = bcrypt.hashSync(password);
     const text = 'INSERT INTO users(name, email) VALUES($1, $2) RETURNING *'
     const values = ['brianc', 'brian.m.carlson@gmail.com']
-    client.query(text, values, (err, res) => {
-        if (err) {
-          console.log(err.stack)
-        } else {
-          console.log(res.rows[0])
-          // { name: 'brianc', email: 'brian.m.carlson@gmail.com' }
-        }
-      })
+    client
+    .query(text, values)
+    .then(res => {
+      console.log(res.rows[0]);
+      System.out.println("Hello, logs!");
+      System.out.println(res.rows[0]);
+      // { name: 'brianc', email: 'brian.m.carlson@gmail.com' }
+    })
+    .catch(e => console.error(e.stack))
 
 })
 
